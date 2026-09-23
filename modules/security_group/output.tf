fe7b6e02f8c9be695ae31df1e218fb7aecdc11e9
Abdulrahman Elhabal <abdelrahmanelhabal910@gmail.com>
2026-09-23 12:45:16 +0300

    refactor: change security group outputs to lists

--- modules/security_group/output.tf
+++ modules/security_group/output.tf
@@ -1,9 +1,15 @@
 output "security_group_ids" {
-  description = "Map of created security group IDs keyed by security group name."
-  value       = { for name, sg in aws_security_group.sg : name => sg.id }
+  description = "List of created security group IDs."
+
+  value = [
+    for sg in aws_security_group.sg : sg.id
+  ]
 }
 
 output "security_group_names" {
-  description = "Map of created security group names keyed by security group name."
-  value       = { for name, sg in aws_security_group.sg : name => sg.name }
+  description = "List of created security group names."
+
+  value = [
+    for sg in aws_security_group.sg : sg.name
+  ]
 }
